fa4df8f826025f061a4ae63a724de466f87b1b26
Mathis PEINGNEZ <mathispeingnez@iMac-Mathis-PEIGNEZ.local>
2020-02-26 18:17:24 +0100

    Authentication not working

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 import { Switch, Route } from 'react-router-dom';
 
+// Components
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Galerie from './components/Galerie';
@@ -11,25 +12,76 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Faune from './components/Faune';
 import AddPhoto from './components/AddPhoto';
+import Login from './components/auth/Login';
+import AuthService from './components/auth/auth-service';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/galerie" component={Galerie} />
-        <Route path="/destinations" component={Destinations} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/add" component={AddPhoto} />
-        <Route path="/faune" component={Faune} />
-      </Switch>
+    this.state = {adminLoggedIn: null};
+    this.service = new AuthService();
+  }
 
-      <Footer />
-    </div>
-  );
+  fetchUser() {
+    if (this.state.adminLoggedIn === null) {
+      this.service.loggedin()
+      .then(res => {
+        this.setState({loggedInUser: res});
+      })
+      .catch(err => {
+        this.setState({loggedInUser: false});
+      });
+    }
+  };
+
+  getTheUser = userObj => {
+    this.setState({
+      loggedInUser: userObj
+    })
+  };
+
+  render() {
+    this.fetchUser();
+    if (this.state.loggedInUser) {
+      return (
+        <div className="App">
+          <Navbar />
+    
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/galerie" component={Galerie} />
+            <Route path="/destinations" component={Destinations} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/add" component={AddPhoto} />
+            <Route path="/faune" component={Faune} />
+            <Route path="/login" component={Login} />
+          </Switch>
+    
+          <Footer />
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <Navbar />
+    
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/galerie" component={Galerie} />
+            <Route path="/destinations" component={Destinations} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/faune" component={Faune} />
+            <Route path="/login" component={Login} />
+          </Switch>
+    
+          <Footer />
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
---
+++ client/src/components/auth/auth-service.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+class AuthService {
+    constructor() {
+        let service = axios.create({
+            baseURL: 'http://localhost:5530/api/auth',
+            withCredentials: true
+        });
+        this.service = service;
+    }
+
+    login = (username, password) => {
+        return this.service.post('/login', {username, password})
+        .then(response => response.data);
+    };
+
+    loggedin = () => {
+        return this.service.get('/loggedin')
+        .then(response => response.data)
+    }
+};
+
+export default AuthService;
