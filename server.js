496b2d390d6b6f5ac0f069393355f8105eeb2f32
Mathis Peingnez <mathis.peingnez@gmail.com>
2020-04-11 17:56:58 +0200

    heroku test

--- server.js
+++ server.js
@@ -72,10 +72,10 @@ app.use('/api/auth', require('./routes/api/admin'));
 //   res.sendFile(__dirname + '/public/index.html');
 // });
 
-// app.use(express.static('/public'));
+app.use(express.static('/public'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/public/index.html'))
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 });
 
 // Port
