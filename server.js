ec3a4fe2a0b3aedb93cda3a142af9a1e8344c95d
Mathis Peingnez <mathis.peingnez@gmail.com>
2020-04-11 17:06:03 +0200

    HEroku edit

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use('/api/auth', require('./routes/api/admin'));
 
 app.use((req, res, next) => {
   // If no routes match, send them the React HTML.
-  res.sendFile(__dirname + '/client/build/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 // Port
