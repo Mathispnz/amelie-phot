5c5ef5a720b475ab1d3a1ba84c324dc41fa5a6b5
Mathis Peingnez <mathis.peingnez@gmail.com>
2020-04-16 17:24:50 +0200

    Cors policy

--- server.js
+++ server.js
@@ -15,8 +15,28 @@ app.use(express.json());
 // Cors to connect to both Client and Server
 app.use(cors({
   credentials: true,
-  origin: ['http://localhost:3000', 'https://ameliepeingnezphotography.herokuapp.com', 'http://ameliepeingnezphotography.herokuapp.com/']
+  origin: ['http://localhost:3000']
 }));
+
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://ameliepeingnezphotography.herokuapp.com/');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 // Connect to the database
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
