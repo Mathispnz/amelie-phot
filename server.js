2c0ba5eff5beec255a569ecb84574d4d331f14d8
Mathis Peingnez <mathis.peingnez@gmail.com>
2020-04-16 17:30:42 +0200

    Fixed url on client

--- server.js
+++ server.js
@@ -20,9 +20,8 @@ app.use(cors({
 
 // Add headers
 app.use(function (req, res, next) {
-
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://ameliepeingnezphotography.herokuapp.com/');
+  res.setHeader('Access-Control-Allow-Origin', 'http://ameliepeingnezphotography.herokuapp.com');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
