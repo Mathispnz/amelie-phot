8dc5942baf0155e74b783692d98698172a876bc7
Mathis Peingnez <mathis.peingnez@gmail.com>
2020-06-01 12:01:35 +0200

    Text about

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -15,7 +15,11 @@ export default function About() {
         Mes parents m’ont offert mon premier appareil photo numérique en 2005 lorsque j’avais 17 ans. Une véritable révélation. J’ai alors passé toutes mes vacances derrière un viseur. Depuis ce jour, j’ai toujours associé le voyage à la photographie et la photographie au voyage.<br /><br />
         C’est 10 ans plus tard que ma vie a pris un tournant quand mon compagnon et moi avons décidé de partir voyager pendant deux ans et demi. A nous la découverte de l’Australie, de l’Indonésie, la Nouvelle-Zélande, Hawaii, l’Ouest Américain, Cuba et le Québec. <br /><br />
         Tous ces paysages que j’ai vu défiler. Ces regards échangés. Ces faunes et ces flores que je découvrais. Ils marquent à jamais.  J’ai vécu ces moments. Je suis ces moments. Je ne pouvais pas garder toutes ces beautés pour moi. J’avais besoin de les partager. <br /><br />
-        Quoi de mieux que la photographie pour le faire ? <br />
+
+        Quoi de mieux que la photographie pour le faire ? <br /><br />
+        Ces deux années passées à voyager à travers le monde m’ont offert l’expérience et la connaissance de la photo de voyage. <br /><br />
+        L’idée de partager ce savoir à travers une activité de coaching en photo de voyage a germé dans ma tête pendant un moment. Les idées c’est bien, mais les réaliser, c’est mieux. <br /><br />
+        Après avoir découvert mon travail photo ici sur Amélie Peingnez Photography, n’hésitez pas à me rejoindre sur Coaching Photo Voyage en cliquant <a href="https://coachingphotovoyage.com/" target="_blank">ici</a>.
         </p>
         <p className="About-Qwig">Amélie</p>
 
