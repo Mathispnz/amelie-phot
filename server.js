dea00f13929858c2085a7851cb3fbc0b0cbb87c2
Mathis Peingnez <mathis.peingnez@gmail.com>
2024-02-24 20:44:09 +0100

    Change MONGODB Database url and host in server

--- server.js
+++ server.js
@@ -1,104 +1,122 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 
-const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 const app = express();
 app.use(express.json());
 
 // Cors to connect to both Client and Server
-app.use(cors({
-  credentials: true,
-  origin: ['http://localhost:3000', 'http://ameliepeingnezphotography.herokuapp.com']
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      "http://localhost:3000",
+      "http://ameliepeingnezphotography.herokuapp.com",
+    ],
+  })
+);
 
 // Connect to the migrated MongoDB Atlas database
+// Encode special characters
+const username = encodeURIComponent(process.env.URI_USERNAME);
+const password = encodeURIComponent(process.env.URI_PASSWORD);
 
-const { MongoClient } = require('mongodb', {useUnifiedTopology: true});
+const { MongoClient } = require("mongodb", { useUnifiedTopology: true });
 // const client = new MongoClient(uri);
 
 async function main() {
-  const uri = process.env.MONGODB_URINO;
+  // const uri = process.env.MONGODB_URINO;
+  const uri = process.env.URI_GENERAL;
 
-  const client = new MongoClient(uri, {useUnifiedTopology: true});
- 
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+
   try {
-      // Connect to the MongoDB cluster
-      await client.connect();
+    // Connect to the MongoDB cluster@
+    await client.connect();
 
-      // Make the appropriate DB calls
-      await listDatabases(client);
-
+    // Make the appropriate DB calls
+    await listDatabases(client);
   } catch (e) {
-      console.error(e);
+    console.error(e);
   } finally {
-      await client.close();
+    await client.close();
   }
 }
 
-async function listDatabases(client){
+async function listDatabases(client) {
   databasesList = await client.db().admin().listDatabases();
 
   console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
 
 main().catch(console.error);
 
 // Connect to the database
-mongoose.connect(process.env.MONGODB_URINO, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-.catch(err => console.log(err));
+mongoose
+  .connect(process.env.URI_MONGOOSE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.log(err));
 
 // Passport
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
-  Admin.findById(id, function(err, user) {
-      done(err, user);
+passport.deserializeUser(function (id, done) {
+  Admin.findById(id, function (err, user) {
+    done(err, user);
   });
 });
 
-passport.use(new LocalStrategy((username, password, next) => {
+passport.use(
+  new LocalStrategy((username, password, next) => {
     Admin.findOne({ username }, (err, foundUser) => {
       if (err) {
         next(err);
         return;
       }
-  
+
       if (!foundUser) {
-        next(null, false, { message: 'Incorrect username.' });
+        next(null, false, { message: "Incorrect username." });
         return;
       }
-  
+
       next(null, foundUser);
     });
-}))
+  })
+);
 
-app.use(session({
-  secret: 'secret key',
-  resave: true,
-  saveUninitialized: true
-}))
+app.use(
+  session({
+    secret: "secret key",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 // Passport authentication
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes
-const photos = require('./routes/api/photos');
+const photos = require("./routes/api/photos");
 
-app.use('/api/photos', photos);
-app.use('/api/photos', require('./routes/api/file-upload-routes'));
-app.use('/api/auth', require('./routes/api/admin'));
+app.use("/api/photos", photos);
+app.use("/api/photos", require("./routes/api/file-upload-routes"));
+app.use("/api/auth", require("./routes/api/admin"));
 
 // app.use((req, res, next) => {
 //   // If no routes match, send them the React HTML.
@@ -106,24 +124,24 @@ app.use('/api/auth', require('./routes/api/admin'));
 // });
 
 if (process.env.NODE_ENV === "production") {
-  const root = require('path').join(__dirname, 'public');
+  const root = require("path").join(__dirname, "public");
   app.use(express.static(root));
   app.get("*", (req, res) => {
-      res.sendFile('index.html', { root });
+    res.sendFile("index.html", { root });
   });
 }
 
-const root = require('path').join(__dirname, 'public');
+const root = require("path").join(__dirname, "public");
 app.use(express.static(root));
 app.get("*", (req, res) => {
-    res.sendFile('index.html', { root });
+  res.sendFile("index.html", { root });
 });
 
 // Port
 const port = process.env.PORT || 5530;
 
 app.listen(port, () => {
-  console.log(`Server started on ${port}`)
+  console.log(`Server started on ${port}`);
 });
 
 module.exports = app;
